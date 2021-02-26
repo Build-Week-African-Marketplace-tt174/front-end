cb530b413f375815c0e99b7f7da5553a0452553f
Alan Mir <alanpaulphotos@gmail.com>
2021-02-25 21:23:46 -0500

    Added image links for team members

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Signup from './components/Signup';
+import About from './components/About';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ function App() {
         <Router>
         <div className="App">
          <Route path='/signup' component={Signup} />
+         <Route path="/about" compoennt={About} />
         </div>
         </Router>
     )
--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -9,19 +9,19 @@ const About = () => {
                 <h3>Front End</h3>
 
                 <div className="team">
-                    <img src="#" alt="Alan" />
+                    <img src="https://ca.slack-edge.com/ESZCHB482-W0143SWM5FG-9ac0871f2e8d-512" alt="Alan" />
                     <h4>Alan Mir</h4>
                     <a href="https://github.com/Veilios" >Github</a>
                 </div>
 
                 <div className="team">
-                    <img src="#" alt="Samantha" />
+                    <img src="https://ca.slack-edge.com/ESZCHB482-U01CKMF2RSS-1cbf73589895-512" alt="Samantha" />
                     <h4>Samantha Johnson</h4>
                     <a href="https://github.com/samanthaj516">Github</a>
                 </div>
 
                 <div className="team">
-                    <img src="https://avatars.githubusercontent.com/u/35966763?s=460&u=75c8887f1818037771798426fe8ee58234e5fd92&v=4" alt="Mark" />
+                    <img src="https://ca.slack-edge.com/ESZCHB482-W012X6T496D-80c9c36cb6f9-512" alt="Mark" />
                     <h4>Mark Stahl</h4>
                     <a href="https://github.com/markstahl">Github</a>
                 </div>
@@ -31,13 +31,13 @@ const About = () => {
                 <h3>Back End</h3>
 
                 <div className="team">
-                    <img src="#" alt="William"></img>
+                    <img src="https://ca.slack-edge.com/ESZCHB482-W015HRCSW3G-be4b82e0b948-512" alt="William"></img>
                     <h4>William Jensen</h4>
                     <a href="https://github.com/william-jensen01">Github</a>
                 </div>
 
                 <div className="team">
-                    <img src="#" alt="Estaban" />
+                    <img src="https://ca.slack-edge.com/ESZCHB482-W012JQ3PDC3-ffb713631186-512" alt="Estaban" />
                     <h4>Estaban Licea Jr.</h4>
                     <a href="https://github.com/e-licea">Github</a>
                 </div>
@@ -45,3 +45,5 @@ const About = () => {
         </div>
     )
 };
+
+export default About;
