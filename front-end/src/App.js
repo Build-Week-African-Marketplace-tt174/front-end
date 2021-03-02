ad4abc53a8f801be0cbddcdde702f06c0eead930
Mark Stahl <markstahl0219@gmail.com>
2021-03-01 20:28:44 -0500

    nav

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Signup from './components/Signup';
 import './App.css';
+import Nav from './components/Nav'
 
 
 function App() {
     return (
         <Router>
         <div className="App">
+        <Nav/>
          <Route path='/signup' component={Signup} />
         </div>
         </Router>
--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import TextField from "@material-ui/core/TextField";
+import styled from "styled-components";
 import Button from "@material-ui/core/Button";
 
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+  margin-left: 33%;
+  text-align: center;
+  margin-top: 20px;
+  padding: 4px;
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const Banner = styled.div`
+background-image: url('https://i.pinimg.com/originals/f4/72/15/f4721513f184bc30f9e6bebf679c431c.jpg');
+background-size: cover;
+background-position: center;
+border-bottom: 3px solid black;
+height: 280px;
+`;
 
 const Signup = props => {
 
     return (
       <div>
+        <Banner />
+        <FormWrapper>
             <h1>Create an Account</h1>
             <TextField
             label="Company Name"
@@ -32,11 +57,12 @@ const Signup = props => {
              placeholder="Password"
              name="password"
             />
+            <ButtonWrapper>
           <Button variant="contained" type="submit">
             Sign Up
           </Button>
-        <div>
-        </div>
+          </ButtonWrapper>
+          </FormWrapper>
         </div>
     );
 };
