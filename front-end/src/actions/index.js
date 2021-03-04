7d1e3c2f8a4b00ce1be28f87ece534a60eeff2fe
Alan Mir <alanpaulphotos@gmail.com>
2021-03-03 23:40:34 -0500

    Downloaded dependencies and set up basic reducer

---
+++ front-end/src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_ITEMS = "FETCH_ITEMS"
+
+export const getItems = () => (dispatch) => {
+    dispatch({type: FETCH_ITEMS});
+    console.log("AM: actions/index.js: Getting information");
+
+    axios.get(`https://africanmarketplace-tt174.herokuapp.com/api/items`)
+        .then(res => {
+            console.log("AM: actions/index.js: Res", res);
+        })
+        .catch(err => console.error("Could not fetch items: ", err));
+};
---
+++ front-end/src/reducers/index.js
@@ -0,0 +1,19 @@
+import { FETCH_ITEMS } from '../actions';
+
+const initialState = {
+    items: [],
+    isFetching: false
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_ITEMS: {
+            return {
+                ...state, isFetching: true
+            }
+        }
+
+        default:
+            return state;
+    }
+};
