af3e4bd6a9ad93ee808facc8a2c2ef5222bdcb94
Mark Stahl <markstahl0219@gmail.com>
2021-03-07 20:16:41 -0500

    styled cards

--- front-end/src/components/ItemCard.js
+++ front-end/src/components/ItemCard.js
@@ -1,38 +1,43 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
 import '../App.css'
 
-
+const ItemContainer = styled.div`
+    display: flex;
+    justify-content: center;
+     align-items: center;
+     margin-top: 40px;
+`;
 
 const Card = styled.div`
-    width: 50%;
-    height: 200px;
-    border: 2px solid black;
-    margin-left: 25%;
+    background: url('https://image.freepik.com/free-vector/traditional-african-art-background_40847-534.jpg');
+    background-position: 50% 50%;
+    height: 300px;
+    border: 4px solid black;
     margin-top: 5px;
+    text-align: center;
+    width: 30%;
+    border-radius: 5px 5px 5px 5px;
+    box-shadow: 0 4px 25px 0 rgba(0,0,0,0.8);
+    transition: 0.3s;
+    font-size: 24px;
+    color: white;
+
+    &:hover {
+        background: url('https://images.assetsdelivery.com/compings_v2/tarapong/tarapong1508/tarapong150800006.jpg');
+        background-position: 50% 50%
+    }
 `;
 
 const Name = styled.p`
-width: 25%;
-border-bottom: 1px solid black;
+    margin-bottom: 60px;
 `;
 
 const Description = styled.p`
-width: 25%;
-border-bottom: 1px solid black;
+    margin-bottom: 40px
 `;
 
 const Price = styled.div`
-display: flex;
-margin-left: 8%;
-justify-content: space-around;
-`;
-
-const Category = styled.p`
-width: 25%;
 `;
 
 
@@ -41,11 +46,13 @@ const ItemCard = (props) => {
     
 
     return (
+        <ItemContainer>
        <Card>
             <Name>Name: {props.item.name}</Name>
             <Description>Description: {props.item.description}</Description>
-            <Price>Price: {props.item.price}</Price>
+            <Price>Price: ${props.item.price}</Price>
        </Card>
+       </ItemContainer>
     )
 }
 
